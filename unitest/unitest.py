921b87b2154fc7abd1039fdcdbecec82063e42fd
Erik <40282726+erik1110@users.noreply.github.com>
2019-08-04 16:53:07 +0800

    Add files via upload

--- unitest/unitest.py
+++ unitest/unitest.py
@@ -5,27 +5,54 @@ from count import Count
 class TestCount(unittest.TestCase):
     
     def setUp(self):
-        print('setUp')
+        #print('setUp')
         self.obj=Count()
 
     def tearDown(self):
-        print('tearDown')
+        #print('tearDown')
         self.obj=None
 
-    def add_test(self):
-        print('runaddTest')
-        print(self.obj.add(10,20)==30)
+    def test_add(self):
+        self.assertEqual(self.obj.add(10,20),30)
     
-    def sub_test(self):
-        print('runsubTest')
-        print(self.obj.add(10,6)==4)
-    
-    # def runTest(self):
-    #     print (self.obj.add(10,20)==30)
+    def test_sub(self):
+        self.assertEqual(self.obj.sub(10,6),4)
+
+    def test_sum(self):
+        pass
+
+    def priv_case(self):
+        pass
+
+#自動生成測試集
+def get_suite():
+    suite=unittest.makeSuite(TestCount,prefix='test')
+    suite.addTest(TestCount('priv_case'))
+    print(suite.countTestCases())
+    return suite
+
+#單一生成測試集
+# def get_suite():
+#     demo_countadd=TestCount('add_test')
+#     demo_countsub=TestCount('sub_test')
+#     suite=unittest.TestSuite()
+#     suite.addTest(demo_countadd)
+#     suite.addTest(demo_countsub)
+#     print(suite)
+#     return suite
+
+#使用__init__來構建測試案例集
+class CountTestSuite(unittest.TestSuite):
+    def __init__(self):
+        case_list=['add_test','sub_test']
+        unittest.TestSuite.__init__(self,map(TestCount,case_list))
+
 
 if __name__ == '__main__':
-    demo_countadd=TestCount('add_test')
-    demo_countadd.run()
-    demo_countsub=TestCount('sub_test')
-    demo_countsub.run()
-
+    #最簡單檢測的方式(須要以test開頭的方法)
+    unittest.main()
+    # s=get_suite()
+    # #s=CountTestSuite()
+    # runner=unittest.TextTestRunner()
+    # print('Count of Case:',s.countTestCases())
+    # runner.run(s)
